504c7f2b2a6644204b38299c46fa84b3d4e052ba
fabian <fabian.froding@gmail.com>
2022-04-01 14:12:03 +0200

    add guxat report to research page, link journal article instead of bsc thesis

--- src/components/PortfolioResearch.vue
+++ src/components/PortfolioResearch.vue
@@ -3,20 +3,20 @@
         <div id="portfolio-research">
                 <div class="portfolio-item">
                     <div class="portfolio-img has-margin-right">
-                        <a href="https://gupea.ub.gu.se/handle/2077/67098" target="_blank">
+                        <a href="https://www.sciencedirect.com/science/article/pii/S0164121222000504?dgcid=author" target="_blank">
                             <img src="../resources/project-images/research-sembsc00.jpg" alt="">
                         </a>
                     </div>
                     <div class="portfolio-description">
-                        <h6>Bsc Thesis</h6>
-                        <h1>The Evolution of Role Stereotypes and Related Design (Anti) Patterns</h1>
+                        <h6>Journal Article</h6>
+                        <h1>Role Stereotypes in Software Designs and their Evolution</h1>
                         <p>
                             This study presents how classes based on the role-stereotypes defined by Wirfs-Brock  change over time in software systems, 
                             and how the occurrence of anti-patterns change over time in relation to these roles.
                         </p>
                         <p class="cta">Highlights: <strong>Software Architecture</strong>, <strong>Software Design Patterns</strong>.</p>
                         <br><br>
-                        <p class="global-link"><a href="https://gupea.ub.gu.se/handle/2077/67098" target="_blank"><i class="fas fa-link"/> The Evolution of Role Stereotypes and Related Design (Anti) Patterns</a></p>
+                        <p class="global-link"><a href="https://www.sciencedirect.com/science/article/pii/S0164121222000504?dgcid=author" target="_blank"><i class="fas fa-link"/> The Evolution of Role Stereotypes and Related Design (Anti) Patterns</a></p>
                     </div>
                 </div>
 
@@ -24,6 +24,32 @@
 
                 <div class="portfolio-item">
                     <div class="portfolio-description has-margin-right">
+                        <h6>Academic Report</h6>
+                        <h1>Analyzing the Crowdfunding Performance of 'Metroidvanias' on Kickstarter</h1>
+                        <p>
+                            An analysis of the performance of metroidvania-categorized campaigns on Kickstarter.com.
+                            Identifies factors that have a statistically significant impact on their amount of funding gained.
+                        </p>
+                        <p class="cta">Highlights: <strong>Statistics</strong>, <strong>Data Analysis</strong>, <strong>Data Visualization</strong>.</p>
+                        <br><br>
+                        <p class="global-link"><a href="./AnalyzingMetroidvaniasKickstarter_FabianFroding.pdf" target="_blank"><i class="far fa-file-pdf"/> Analyzing the Crowdfunding Performance of 'Metroidvanias' on Kickstarter.pdf</a></p>
+                    </div>
+                    <div class="portfolio-img">
+                        <a href="./AnalyzingMetroidvaniasKickstarter_FabianFroding.pdf" target="_blank">
+                            <img src="../resources/project-images/research-guxat00.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="separator-line"></div>
+
+                <div class="portfolio-item">
+                    <div class="portfolio-img has-margin-right">
+                        <a href="./InvestigatingVideoGameDevelopProblems_Fabian_Froding.pdf" target="_blank">
+                            <img src="../resources/project-images/research-ege00.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="portfolio-description">
                         <h6>Academic Report</h6>
                         <h1>Investigating Video Game Development Problems Among Genres and Platforms</h1>
                         <p>
@@ -34,22 +60,12 @@
                         <br><br>
                         <p class="global-link"><a href="./InvestigatingVideoGameDevelopProblems_Fabian_Froding.pdf" target="_blank"><i class="far fa-file-pdf"/> Investigating Video Game Development Problems Among Genres and Platforms.pdf</a></p>
                     </div>
-                    <div class="portfolio-img">
-                        <a href="./InvestigatingVideoGameDevelopProblems_Fabian_Froding.pdf" target="_blank">
-                            <img src="../resources/project-images/research-ege00.jpg" alt="">
-                        </a>
-                    </div>
                 </div>
 
                 <div class="separator-line"></div>
 
                 <div class="portfolio-item">
-                    <div class="portfolio-img has-margin-right">
-                        <a href="./PlaytestStudyWarcryYCJY_Fabian_Froding.pdf" target="_blank">
-                            <img src="../resources/project-images/research-agux00.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="portfolio-description">
+                    <div class="portfolio-description has-margin-right">
                         <h6>Academic Report</h6>
                         <h1>Games User Experience - An Experimental Playtest-study With two External Studios</h1>
                         <p>
@@ -59,13 +75,22 @@
                         <br><br>
                         <p class="global-link"><a href="./PlaytestStudyWarcryYCJY_Fabian_Froding.pdf" target="_blank"><i class="far fa-file-pdf"/> Games User Experience - An experimental playtest-study with two external studios.pdf</a></p>
                     </div>
-                    
+                    <div class="portfolio-img">
+                        <a href="./PlaytestStudyWarcryYCJY_Fabian_Froding.pdf" target="_blank">
+                            <img src="../resources/project-images/research-agux00.jpg" alt="">
+                        </a>
+                    </div>
                 </div>
 
                 <div class="separator-line"></div>
 
                 <div class="portfolio-item">
-                    <div class="portfolio-description has-margin-right">
+                    <div class="portfolio-img has-margin-right">
+                        <a href="./ExploringTimeLoopSystems_Fabian_Froding.pdf" target="_blank">
+                            <img src="../resources/project-images/research-guxrd00.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="portfolio-description">
                         <h6>Academic Report</h6>
                         <h1>Exploring Ways to Evaluate the Effect of Time-loop Systems on Player Experience in Video Games</h1>
                         <p>
@@ -76,12 +101,6 @@
                         <br><br>
                         <p class="global-link"><a href="./ExploringTimeLoopSystems_Fabian_Froding.pdf" target="_blank"><i class="far fa-file-pdf"/> Exploring ways to evaluate the effect of time-loop systems on player experience in video games.pdf</a></p>
                     </div>
-                    <div class="portfolio-img">
-                        <a href="./ExploringTimeLoopSystems_Fabian_Froding.pdf" target="_blank">
-                            <img src="../resources/project-images/research-guxrd00.jpg" alt="">
-                        </a>
-                    </div>
-                    
                 </div>
 
                 <div class="separator-line"></div>
