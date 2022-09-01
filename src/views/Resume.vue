b0522c6796a58b4c1e21dc53d8e2f12a54a1701b
fabian <fabian.froding@gmail.com>
2022-09-02 00:26:36 +0200

    add some spacing to recommendatio texts

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -24,6 +24,7 @@
                         <p><i>"I can warmly recommend Fabian for different tasks in QA. 
                             He was of great help and took responsibility to ensure that the work went smooth and reached a high quality during the several weeks of the collaboration. 
                             He was attentive to our needs and wishes and made sure that the group collaborated to produce a clear and in-depth material of test data for our game project. 
+                            <br><br>
                             The work consisted of finding test-participants and also to produce interview-questions and compile the results from these test-participants' testing of the game we're developing. 
                             Subsequently an analysis and conclusions were made that were insightful and the result was presented in a well-made presentation. 
                             Fabian shows that he takes the task seriously and shows that a passion exists for QA and to reach very good results."</i>
@@ -47,19 +48,20 @@
                 </div>
                 <div class="my-skills">
                     <div class="skill">
+                        <p><i>"This is to certify that Mr. Fabian Froding has been employed as a part-time (50%) software engineer at Evoma AB during 15 March to 31 December 2021. 
+                            During this part-time project employment period, he has assisted our software team in developing a customized simulation-optimization software for a major European aerospace manufacturer. 
+                            <br><br>
+                            In particular, he was responsible for customizing a JavaScript-based reporting module for the users to visualize and interact with large volumes of simulation-optimization output data. 
+                            Despite this part-time project employment was short, he has completed the tasks assigned to him, diligently and independently, to our satisfaction."</i> 
+                        <br><br>- <strong>Amos Ng, CEO at Evoma AB.</strong></p><p class="global-link"><a href="resources/QufuShaolinSchool_Certificate.pdf" target="_blank"></a></p>
+                        <!--p class="global-link"><a href="./ReferenceLetter_Fabian_Froding_Evoma_2021.pdf" target="_blank"><i class="far fa-file-pdf"/> Evoma Reference Letter.pdf</a></p-->
+                    </div>
+                    <div class="skill">
                         <p><i>"Fabian has trained under the Shaolin 34th generation Warrior Monk, Master Shi Yan Jia, for the duration of 6 months on Shaolin Kung Fu and Tai Chi etc.
                         He trained very hard and was respectful to all of the masters in this periods starts from June, 2013 to December, 2013.
                         He is an honest and excellent student."</i> 
                         <br><br>- <strong>Master Shi Yan Jia, 34th generation Shaolin Warrior Monk at Qufu Shaolin Kung Fu School.</strong></p><p class="global-link"><a href="resources/QufuShaolinSchool_Certificate.pdf" target="_blank"></a></p>
                         <!--p class="global-link"><a href="./QufuShaolinSchoolCertificate_Fabian_Froding.pdf" target="_blank"><i class="far fa-file-pdf"/> Certificate from Qufu Shaolin Kung Fu School.pdf</a></p-->
-                    </div>
-                    <div class="skill">
-                        <p><i>"This is to certify that Mr. Fabian Froding has been employed as a part-time (50%) software engineer at Evoma AB during 15 March to 31 December 2021. 
-                            During this part-time project employment period, he has assisted our software team in developing a customized simulation-optimization software for a major European aerospace manufacturer. 
-                            In particular, he was responsible for customizing a JavaScript-based reporting module for the users to visualize and interact with large volumes of simulation-optimization output data. 
-                            Despite this part-time project employment was short, he has completed the tasks assigned to him, diligently and independently, to our satisfaction."</i> 
-                        <br><br>- <strong>Amos Ng, CEO at Evoma AB.</strong></p><p class="global-link"><a href="resources/QufuShaolinSchool_Certificate.pdf" target="_blank"></a></p>
-                        <!--p class="global-link"><a href="./ReferenceLetter_Fabian_Froding_Evoma_2021.pdf" target="_blank"><i class="far fa-file-pdf"/> Evoma Reference Letter.pdf</a></p-->
                     </div>
                 </div>
             </div>
