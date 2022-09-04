0113832cdf3148d4bdd7c5137c54f7888008abf9
fabian <fabian.froding@gmail.com>
2022-09-04 11:00:04 +0200

    update about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,7 +14,7 @@
 					<p>
 						My time in the industry and academia have contributed to a diverse yet useful set of skills mainly consisting of 
 						<strong>software engineering</strong> and <strong>game development</strong>, with a specialization in 
-						<strong>software architecture</strong>, <strong>Unity</strong> and <strong>Unreal Engine</strong>.
+						<strong>software architecture</strong> and <strong>AI for games</strong>.
                     </p>
                     <br>
 					<div class="separator-line"></div>
@@ -22,7 +22,7 @@
 					<p>
                         My journey in game development began at the age of 13 when I discovered the Warcraft 3 World Editor.
 						Since then, I've had somewhat of an obsession with creating things.
-						Now, I'm experienced in both Unity and Unreal Engine.
+						Now, I'm experienced in both <strong>Unity</strong> and <strong>Unreal Engine</strong>.
 						<br>I occasionally like to play my favourite games and try out new ones, 
 						but most of my time I prefer to <strong>develop things</strong>, 
 						be it programming complex gameplay systems or creating immersive experiences.
@@ -44,21 +44,21 @@
 						<ul>
 							<li><h2>Some of my favourite games include:</h2></li>
 							<br>
-							<li><p> • Hollow Knight</p></li>
-							<li><p> • Subnautica</p></li>
 							<li><p> • Metroid</p></li>
+							<li><p> • Subnautica</p></li>
 							<li><p> • The Legend of Zelda</p></li>
-							<li><p> • Banjo-Kazooie/Tooie</p></li>
-							<li><p> • Donkey Kong</p></li>
+							<li><p> • Hollow Knight</p></li>
+							<li><p> • Banjo-Kazooie</p></li>
 							<li><p> • Little Nightmares</p></li>
+							<li><p> • Donkey Kong</p></li>
 							<li><p> • Ori and the Blind Forest</p></li>
 							<li><p> • Celeste</p></li>
 							<li><p> • Dead Cells</p></li>
-							<li><p> • Shadow of the Colossus</p></li>
-							<li><p> • The Last Guardian</p></li>
 							<li><p> • Abzu</p></li>
 							<li><p> • Octopath Traveler</p></li>
+							<li><p> • Tales of Symphonia</p></li>
 							<li><p> • Warcraft 3</p></li>
+							<li><p> • World of Warcraft</p></li>
 						</ul>
                     </div>
 
@@ -68,11 +68,10 @@
 							<h2>Outside of my time spent on development, I occasionally occupy myself with:</h2>
 							<br>
 							<li><p> • Practicing martial arts</p></li>
-							<li><p> • Reading books about psychology, philosophy, health and entrepreneurship</p></li>
-							<li><p> • Animals</p></li>
+							<li><p> • Reading books about entrepreneurship, psychology, philosophy, and health</p></li>
 							<li><p> • Watching immersive movies &amp; series</p></li>
 							<li><p> • Cooking</p></li>
-							<li><p> • Running</p></li>
+							<li><p> • Exercising</p></li>
 							<li><p> • Learning from game developers on Youtube</p></li>
 							<!--li>
 								<ul>
