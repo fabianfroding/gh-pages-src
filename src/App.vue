828da88f229811d058278d2b083c97096240ca9d
Fabian F <fabian.froding@gmail.com>
2025-06-07 19:02:43 +0200

    Major layout updates
     - Updated layouts
     - Removed various tabs

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div id="app">
     <nav class="top-nav">
-        <ul class="nav-list">
-            <li>
-                <router-link class="nav-link" to="/games">Games</router-link>
-            </li>
-            <li>
-                <router-link class="nav-link" to="/software">Software</router-link>
-            </li>
-            <li id="nav-research">
-                <router-link class="nav-link" to="/research">Research</router-link>
-            </li>
-            <li id="nav-services">
-                <router-link class="nav-link" to="/resume">Resume</router-link>
-            </li>
-            <li id="nav-about">
-                <router-link class="nav-link" to="/about">About</router-link>
-            </li>
-            <li>
-                <router-link class="nav-link" to="/contact">Contact</router-link>
-            </li>
-        </ul>
+            <ul class="nav-list">
+                <li><router-link class="nav-link" to="/games">Games</router-link></li>
+                <li><router-link class="nav-link" to="/software">Software</router-link></li>
+                <li id="nav-about"><router-link class="nav-link" to="/about">About</router-link></li>
+                <li><router-link class="nav-link" to="/contact">Contact</router-link></li>
+            </ul>
     </nav>
     <header>
         <div class="landing-text" id="landing-text">
             <h1>Fabian Fröding</h1>
-            <h6>Game Programmer & Software Engineer</h6>
+            <h6>Software Engineer &amp; Game Developer</h6>
         </div>
     </header>
     <div class="separator-line"></div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -30,7 +30,7 @@
 						<h1>About</h1>
 						<h6>Me</h6>
 					</div>
-					<p>Hello! I'm Fabian, a game programmer and software engineer with a passion for modular and scalable software design.
+					<p>Hello! I'm Fabian, a software engineer and game developer with a passion for modular and scalable software design.
 						My development skills are quite versatile, with core strengths in <strong>Unreal Engine &amp; C++</strong>, <strong>Unity &amp; C#</strong>, and <strong>Web Development</strong>.
                     </p>
                     <br>
@@ -50,50 +50,101 @@
                 </div>
 			</div>
 		</section>
-		<!--div class="separator-line"></div>
+
+		<div class="separator-line"></div>
+
+		<section class="services" id="resume">
+			<div class="container">
+				<div class="about-details">
+					<div class="skill">
+						<h1>Certifications</h1>
+					</div>
+					<div class="my-skills">
+						<div class="skill">
+							<a href="https://www.udemy.com/certificate/UC-931b4d8b-d726-40f6-9f7a-c359d2577c1a/" target="_blank">
+								<div class="icon-container">
+									<i target="_blank" class="fa-brands fa-angular"></i>
+								</div>
+							</a>
+							<a href="https://www.udemy.com/certificate/UC-931b4d8b-d726-40f6-9f7a-c359d2577c1a/" target="_blank">
+								<h1>Angular.JS</h1>
+							</a>
+						</div>
+						<div class="skill">
+							<a href="https://www.udemy.com/certificate/UC-49481515-4977-4768-9795-882048bb634b/" target="_blank">
+								<div class="icon-container">
+									<i class="fa-solid fa-gamepad"></i>
+								</div>
+							</a>
+							<a href="https://www.udemy.com/certificate/UC-49481515-4977-4768-9795-882048bb634b/" target="_blank">
+								<h1>Unreal Engine - Gameplay Ability System</h1>
+							</a>
+						</div>
+                    </div>
+				</div>
+
+				<!--div class="about-details">
+					<div class="skill">
+						<h1>Game Development Skills</h1>
+					</div>
+				</div-->
+			</div>
+        </section>
+
+		<div class="separator-line"></div>
+
+		<section class="services" id="resume">
             <div class="container">
-				<div class="my-skills">
+                <div class="my-skills">
                     <div class="skill">
-                        <h1>Hobbies and Interests</h1>
+                        <h1>Recommendations</h1>
                     </div>
                 </div>
                 <div class="my-skills">
-                    <div class="skill fav-games">
-						<ul>
-							<li><h2>While I spend most of my time developing things, I occasionally enjoy trying new indie games, and sometimes returning to my favourites:</h2></li>
-							<br>
-							<li><p> • Banjo-Kazooie</p></li>
-							<li><p> • Donkey Kong</p></li>
-							<li><p> • Fire Emblem</p></li>
-							<li><p> • Hollow Knight</p></li>
-							<li><p> • Metroid</p></li>
-							<li><p> • Octopath Traveler</p></li>
-							<li><p> • Ori and the Blind Forest</p></li>
-							<li><p> • Subnautica</p></li>
-							<li><p> • Tales of Symphonia</p></li>
-							<li><p> • The Legend of Zelda</p></li>
-							<li><p> • Warcraft III</p></li>
-							<li><p> • World of Warcraft</p></li>
-						</ul>
+                    <div class="skill">
+                        <p><i>"I can warmly recommend Fabian for different tasks in QA. 
+                            He was of great help and took responsibility to ensure that the work went smooth and reached a high quality during the several weeks of the collaboration. 
+                            He was attentive to our needs and wishes and made sure that the group collaborated to produce a clear and in-depth material of test data for our game project. 
+                            <br><br>
+                            The work consisted of finding test-participants and also to produce interview-questions and compile the results from these test-participants' testing of the game we're developing. 
+                            Subsequently an analysis and conclusions were made that were insightful and the result was presented in a well-made presentation. 
+                            Fabian shows that he takes the task seriously and shows that a passion exists for QA and to reach very good results."</i>
+                        <br><br>- <strong>Dan Pettersson, VD at Warcry Interactive.</strong></p>
                     </div>
-
-                    <div class="skill fav-games">
-                        <ul>
-							<h2>In my spare time, I occasionally occupy myself with:</h2>
-							<br>
-							<li><p> • Cinematography and storytelling.</p></li>
-							<li><p> • Exercising.</p></li>
-							<li><p> • Following tech news.</p></li>
-							<li><p> • Martial arts.</p></li>
-							<li><p> • Modding.</p></li>
-							<li><p> • Reading novels, non-fiction books and poetry.</p></li>
-							<li><p> • 2D art as a recreational activity.</p></li>
-						</ul>
+                    <div class="skill">
+                        <p><i>"Fabian has trained under the Shaolin 34th generation Warrior Monk, Master Shi Yan Jia, for the duration of 6 months on Shaolin Kung Fu and Tai Chi etc.
+                        He trained very hard and was respectful to all of the masters in this periods starts from June, 2013 to December, 2013.
+                        He is an honest and excellent student."</i> 
+                        <br><br>- <strong>Master Shi Yan Jia, 34th generation Shaolin Warrior Monk at Qufu Shaolin Kung Fu School.</strong></p><p class="global-link"><a href="resources/QufuShaolinSchool_Certificate.pdf" target="_blank"></a></p>
                     </div>
-
+                </div>
+                <div class="my-skills">
+                    <div class="skill">
+                        <p><i>"This is to certify that Mr. Fabian Froding has been employed as a part-time (50%) software engineer at Evoma AB during 15 March to 31 December 2021. 
+                            During this part-time project employment period, he has assisted our software team in developing a customized simulation-optimization software for a major European aerospace manufacturer. 
+                            <br><br>
+                            In particular, he was responsible for customizing a JavaScript-based reporting module for the users to visualize and interact with large volumes of simulation-optimization output data. 
+                            Despite this part-time project employment was short, he has completed the tasks assigned to him, diligently and independently, to our satisfaction."</i> 
+                        <br><br>- <strong>Amos Ng, CEO at Evoma AB.</strong></p><p class="global-link"><a href="resources/QufuShaolinSchool_Certificate.pdf" target="_blank"></a></p>
+                    </div>
+                    <div class="skill">
+                        <p><i>"I have worked with Fabian on projects during our Bachelor’s in Software Engineering as well as on a project in Unity since March 2021. 
+                            I find that little fazes Fabian - when approaching a complex problem, he calmly tries to understand the problem, then forms a plan to solve it and executes it effectively. 
+                            His eagerness to learn, enthusiasm and dedication to his craft can be seen when he continually tries to improve the code architecture, grasp new concepts, and learn new tools quickly.
+                            <br><br>
+                            Fabian can always be relied on to work independently, delivering quality work. 
+                            He is often proactive, unafraid to bring up issues or his thoughts when he believes it is necessary. 
+                            During our academic projects, his strong work ethic was also shown when put under pressure, completing his tasks within tight deadlines.
+                            <br><br>
+                            Fabian’s sense of maturity is not only shown when dealing with technical issues, but also with his tact and consideration in human relationships. 
+                            This has not only contributed to a sense of safety and fun in a work atmosphere, but also a voice of insight and reason as a friend. 
+                            I believe Fabian can be a core asset to any team he wishes to be part of. As such, I can confidently recommend him."</i> 
+                        <br><br>- <strong>Nigel Chao, Software Engineer.</strong></p><p class="global-link"><a href="resources/QufuShaolinSchool_Certificate.pdf" target="_blank"></a></p>
+                    </div>
                 </div>
             </div>
-			<br><br><br><br><br-->
+        </section>
+
 		<Footer :showArrow="true"></Footer>
 	</div>
 </template>
